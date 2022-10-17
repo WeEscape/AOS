c05f57f953d5540b4e48e6dbca888f823d6c4f33
Nahyun Lee <lnhyen43@gmail.com>
2022-10-18 00:50:17 +0900

    CHORE: 프리티어, eslint 초기 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "react-native/react-native": true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      tsx: true,
+      ts: true,
+      jsx: true,
+      js: true,
+    },
+  },
+  plugins: ["react", "react-native", "@typescript-eslint"],
+  rules: {
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/sort-styles": [
+      "error",
+      "asc",
+      {
+        ignoreClassNames: false,
+        ignoreStyleProperties: false,
+      },
+    ],
+    // allow .js files to contain JSX code
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", "tsx"] },
+    ],
+    "import/prefer-default-export": "off",
+  },
+};
