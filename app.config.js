6b2a358fa5d4745a13d8eececfbd9456e412c298
Nahyun Lee <lnhyen43@gmail.com>
2022-10-23 18:39:54 +0900

    CHORE: eas.json 환경변수 설정@@@@ 추후에 수정 필요할 수 있음

---
+++ app.config.js
@@ -0,0 +1,33 @@
+module.exports = ({ config }) => {
+  if (process.env.APP_ENV === 'production') {
+    console.log('config1', config);
+    console.log('APP_ENV1', process.env.APP_ENV);
+    return {
+      ...config,
+      extra: {
+        apiUrl: process.env.API_URL,
+        eas: {
+          projectId: '8c6e9c7d-cd83-4f3a-88a0-9f7571a13f42',
+        },
+      },
+    };
+  } else if (process.env.APP_ENV === 'development') {
+    console.log('config2', config);
+    console.log('APP_ENV3', process.env.APP_ENV);
+    return {
+      ...config,
+      extra: {
+        apiUrl: process.env.API_URL,
+        eas: {
+          projectId: '8c6e9c7d-cd83-4f3a-88a0-9f7571a13f42',
+        },
+      },
+    };
+  } else {
+    console.log('config3', config);
+    console.log('APP_ENV3', process.env.APP_ENV);
+    return {
+      ...config,
+    };
+  }
+};
--- index.js
+++ index.js
@@ -3,4 +3,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+console.log('app', appName);
+
 AppRegistry.registerComponent(appName, () => App);
