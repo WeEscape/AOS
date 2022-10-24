bee3587917e45ba23ad4db6d0ac7c69ceb9583b1
Nahyun Lee <lnhyen43@gmail.com>
2022-10-25 00:36:25 +0900

    DESIGN: SignInScreen 카카오 버튼 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "react-native/react-native": true,
+    'react-native/react-native': true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
       tsx: true,
       ts: true,
@@ -17,25 +17,22 @@ module.exports = {
       js: true,
     },
   },
-  plugins: ["react", "react-native", "@typescript-eslint"],
+  plugins: ['react', 'react-native', '@typescript-eslint'],
   rules: {
-    "react-native/no-unused-styles": 2,
-    "react-native/split-platform-components": 2,
-    "react-native/no-inline-styles": 2,
-    "react-native/no-color-literals": 2,
-    "react-native/sort-styles": [
-      "error",
-      "asc",
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    // 'react-native/no-inline-styles': 2,
+    // "react-native/no-color-literals": 2,
+    'react-native/sort-styles': [
+      'error',
+      'asc',
       {
         ignoreClassNames: false,
         ignoreStyleProperties: false,
       },
     ],
     // allow .js files to contain JSX code
-    "react/jsx-filename-extension": [
-      1,
-      { extensions: [".js", ".jsx", ".ts", "tsx"] },
-    ],
-    "import/prefer-default-export": "off",
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', 'tsx'] }],
+    'import/prefer-default-export': 'off',
   },
 };
--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,35 +1,34 @@
 import React from 'react';
-import { StyleSheet, ActivityIndicator, Button, View } from 'react-native';
-import { Image } from '@rneui/themed';
-import { Text } from '@rneui/themed';
+import { StyleSheet, ActivityIndicator, View } from 'react-native';
+import { Image, Text, Button, Icon } from '@rneui/themed';
 import IconImage from '../../assets/icon/logo.png';
-import {
-  useFonts,
-  NotoSansKR_100Thin,
-  NotoSansKR_300Light,
-  NotoSansKR_400Regular,
-  NotoSansKR_500Medium,
-  NotoSansKR_700Bold,
-  NotoSansKR_900Black,
-} from '@expo-google-fonts/noto-sans-kr';
 
 const $colorWhite = '#ffffff';
 const $TITLEFONTCOLOR = '#414141';
 
 const SignInScreen = () => {
-  const [fontsLoaded] = useFonts({
-    NotoSansKR_100Thin,
-    NotoSansKR_300Light,
-    NotoSansKR_400Regular,
-    NotoSansKR_500Medium,
-    NotoSansKR_700Bold,
-    NotoSansKR_900Black,
-  });
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={IconImage} />
       <Text style={styles.text}>가사 분담에서 해방하다</Text>
       <Text style={styles.EnText}>DOBBY</Text>
+      <Button
+        title="카카오로 계속하기"
+        buttonStyle={{
+          backgroundColor: 'rrgb(247, 230, 0)',
+          borderRadius: 12,
+        }}
+        titleStyle={{ color: '#414141', fontFamily: 'Noto Sans KR', fontSize: 14, fontWeight: '700' }}
+        containerStyle={{
+          width: 300,
+          marginHorizontal: 50,
+          marginVertical: 15,
+        }}
+      >
+        {/* 카카오 아이콘 넣기 */}
+        <Icon name="home" color="white" />
+        카카오로 계속하기
+      </Button>
     </View>
   );
 };
@@ -55,7 +54,6 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     width: 50,
   },
-
   text: {
     fontFamily: 'NotoSansKR_500Medium',
     fontWeight: '500',
